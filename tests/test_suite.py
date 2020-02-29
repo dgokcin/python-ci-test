a7d652be9e5f4fb268622701c67418c8c169bb96
Deniz Gökçin <dgokcin@gmail.com>
2020-02-29 14:16:10 +0300

    really

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,6 +1,8 @@
 import unittest
 
+
 class TestSum(unittest.TestCase):
+
 
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
@@ -10,4 +12,3 @@ class TestSum(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
